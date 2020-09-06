0114df7ce5845fe369cedded486af06a2fe667fd
Marcin Harasim <marcin.sopot@gmail.com>
2020-09-06 12:22:36 +0200

    after added Genre, Director python manage.py makemigrations python manage.py migrate

--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -2,15 +2,48 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
+AGE_CHOICES = (
+    (7, 'Children'),
+    (12, 'Young'),
+    (18, 'Adult'),
+    (50, 'Retired')
+)
+# class AgeLimit(models.IntegerChoices):
+#     Adult = 18
+#     Young = 12
+#     Children = 0
+#
+#
+# agelimit = models.IntegerField(choices=AgeLimit.choices, null=True, blank=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    age_limit = models.IntegerField(choices=AGE_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Director(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=70)
+
+    class Meta:
+        unique_together = ('name', 'surname')
+    def __str__(self):
+        return f'{self.name} {self.surname}'
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField(
         null=True, validators=[MaxValueValidator(10), MinValueValidator(1)]
     )
-    released = models.DateField()
+    category = models.CharField(null=True, max_length=100)
+    released = models.DateField(null=True)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.SET_NULL)
+    director = models.ForeignKey(Director, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.title} from {self.released}"
